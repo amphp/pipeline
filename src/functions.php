11bb56a904fd92692e5f175c2c4519601a91e4b3
Aaron Piotrowski <aaron@trowski.com>
2022-01-15 09:00:24 -0600

    Remove dependency on amphp/sync
    Dependency will be inverted, instead amphp/sync will depend on this library.

--- src/functions.php
+++ src/functions.php
@@ -195,23 +195,6 @@ function zip(array $pipelines): Pipeline
 }
 
 /**
- * Concurrently act on a pipeline using the given set of operators. The resulting pipeline will *not* necessarily be
- * in the same order as the source pipeline, however, items are emitted as soon as they are available.
- *
- * @template TValue
- * @template TResult
- *
- * @param Semaphore $semaphore Semaphore limiting the concurrency, e.g. {@see LocalSemaphore}.
- * @param PipelineOperator ...$operators Set of operators to act upon each value emitted. See {@see Pipeline::pipe()}.
- *
- * @return PipelineOperator<TValue, TResult>
- */
-function concurrent(Semaphore $semaphore, PipelineOperator ...$operators): PipelineOperator
-{
-    return new Internal\Operator\ConcurrentOperator($semaphore, $operators);
-}
-
-/**
  * Removes backpressure on the source pipeline.
  *
  * @template TValue
