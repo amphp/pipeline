3a22f91e509001f1f0f1ef32df7f44ddba7625a8
Aaron Piotrowski <aaron@trowski.com>
2022-01-30 13:05:13 -0600

    Ensure ordering from generate()

--- src/Internal/ConcurrentClosureIterator.php
+++ src/Internal/ConcurrentClosureIterator.php
@@ -3,10 +3,9 @@
 namespace Amp\Pipeline\Internal;
 
 use Amp\Cancellation;
-use Amp\CancelledException;
+use Amp\DeferredCancellation;
 use Amp\Pipeline\ConcurrentIterator;
-use Amp\Pipeline\DisposedException;
-use Revolt\EventLoop\FiberLocal;
+use Revolt\EventLoop;
 
 /**
  * @internal
@@ -18,70 +17,110 @@ final class ConcurrentClosureIterator implements ConcurrentIterator
 {
     private \Closure $supplier;
 
-    /** @var FiberLocal<T|null> */
-    private FiberLocal $currentValue;
+    private \SplQueue $sources;
 
-    /** @var FiberLocal<int|null> */
-    private FiberLocal $currentPosition;
+    private QueueState $queue;
+
+    private Sequence $sequence;
+
+    private DeferredCancellation $deferredCancellation;
+
+    private int $cancellations = 0;
 
     private int $position = 0;
-
-    private ?\Throwable $exception = null;
 
     public function __construct(\Closure $supplier)
     {
         $this->supplier = $supplier;
-        $this->currentValue = new FiberLocal(fn () => throw new \Error('Call continue() before calling get()'));
-        $this->currentPosition = new FiberLocal(fn () => throw new \Error('Call continue() before calling get()'));
+        $this->sequence = new Sequence();
+        $this->queue = new QueueState();
+        $this->sources = new \SplQueue();
+        $this->deferredCancellation = new DeferredCancellation();
     }
 
     public function continue(?Cancellation $cancellation = null): bool
     {
-        if ($this->exception) {
-            throw $this->exception;
+        if ($this->queue->isComplete()) {
+            return $this->queue->continue($cancellation);
+        }
+
+        if ($this->cancellations) {
+            --$this->cancellations;
+            return $this->queue->continue($cancellation);
+        }
+
+        if ($this->sources->isEmpty()) {
+            $queue = $this->queue;
+            $sources = $this->sources;
+            $sequence = $this->sequence;
+            $supplier = $this->supplier;
+            $deferredCancellation = $this->deferredCancellation;
+            EventLoop::queue(static function (int $position) use (
+                $queue,
+                $sources,
+                $sequence,
+                $supplier,
+                $deferredCancellation
+            ): void {
+                $suspension = EventLoop::getSuspension();
+
+                do {
+                    try {
+                        $value = $supplier($deferredCancellation->getCancellation());
+                    } catch (\Throwable $exception) {
+                        $sequence->await($position);
+                        if (!$queue->isComplete()) {
+                            $queue->error($exception);
+                            $deferredCancellation->cancel($exception);
+                        }
+                        $sequence->resume($position);
+                    } finally {
+                        $sources->enqueue($suspension);
+                    }
+
+                    $sequence->await($position);
+                    if (!$queue->isComplete()) {
+                        $queue->push($value);
+                    }
+                    $sequence->resume($position);
+                } while ($position = $suspension->suspend());
+            }, $this->position++);
+        } else {
+            $suspension = $this->sources->dequeue();
+            $suspension->resume($this->position++);
+        }
+
+        if ($cancellation) {
+            $cancellations = &$this->cancellations;
+            $id = $cancellation->subscribe(static function () use (&$cancellations): void {
+                ++$cancellations;
+            });
         }
 
         try {
-            $value = ($this->supplier)($cancellation);
-            $this->currentValue->set($value);
-            $position = $this->position++;
-            $this->currentPosition->set($position);
-        } catch (CancelledException $cancelledException) {
-            throw $cancelledException;
-        } catch (\Throwable $exception) {
-            $this->currentValue->set(null);
-            $this->currentPosition->set(null);
-            $this->exception = $exception;
-
-            throw $exception;
+            return $this->queue->continue($cancellation);
+        } finally {
+            $cancellation?->unsubscribe($id);
         }
-
-        return true;
     }
 
     public function getValue(): mixed
     {
-        $position = $this->currentPosition->get();
-        if ($position === null) {
-            throw new \Error('continue() returned false, no value available afterwards');
-        }
-
-        return $this->currentValue->get();
+        return $this->queue->getValue();
     }
 
     public function getPosition(): int
     {
-        $position = $this->currentPosition->get();
-        if ($position === null) {
-            throw new \Error('continue() returned false, no position available afterwards');
-        }
-
-        return $position;
+        return $this->queue->getPosition();
     }
 
     public function dispose(): void
     {
-        $this->exception ??= new DisposedException;
+        while (!$this->sources->isEmpty()) {
+            $this->sources->dequeue();
+        }
+
+        $this->queue->dispose();
     }
 
     public function getIterator(): \Traversable
